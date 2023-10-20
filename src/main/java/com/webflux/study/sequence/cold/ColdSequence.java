c1016ea5fcd3e8fbc67b028b1040a0267d6287ef
KimHyunJin <khj745700@naver.com>
2023-10-20 17:52:04 +0900

    Study : Cold Sequence, Hot Sequence Momo 추가

--- src/main/java/com/webflux/study/sequence/cold/ColdSequence.java
+++ src/main/java/com/webflux/study/sequence/cold/ColdSequence.java
@@ -12,6 +12,12 @@ public class ColdSequence {
          * Cold 는 처음부터 새로 시작해야 하며 같은 작업이 반복되는 의미를 지니고 있음.
          *
          */
+
+        /**
+         * Memo
+         * Subscriber 의 구독 시점이 달라도 구독을 할 때마다 Publisher 가 데이터를 처음부터 emit 하는 과정을 Cold Sequence 라 함.
+         * Cold Sequence 흐름을 동작하는 Publisher 를 Cold Publisher 라 함.
+         */
         Flux<String> coldFlux =
                 Flux
                         .fromIterable(Arrays.asList("KOREA", "JAPAN", "CHINESE")) // fromIterable() 메서드는 Iterable 을 상속받은 Collection 들의 데이터를 Emit 하는 메서드
--- src/main/java/com/webflux/study/sequence/hot/HotSequence.java
+++ src/main/java/com/webflux/study/sequence/hot/HotSequence.java
@@ -11,6 +11,14 @@ public class HotSequence {
         /**
          * Hot 은 처음부터 다시 시작하지 않고 같은 작업이 반복되지 않는 느낌.
          */
+
+        /**
+         * Memo
+         * Publisher 가 데이터를 emit 하는 과정이 한 번만 일어나고, Subscriber 가 각각의 구독 시점 이후에 emit 된 데이터만 전달받는 것을 Hot Sequence 라고 함.
+         * Hot Sequence 흐름으로 동작하는 Publisher 를 Hot Publisher 라고 함.
+         * share(), cache() 등의 Operator 를 사용해서 Cold Sequence 를 Hot Sequence 로 변환할 수 있음.
+         * Hot Sequence 는 Subscriber 의 최초 구독이 발생 해야 Publisher 가 데이터를 emit 하는 Warm up 과 Subscriber 의 구독 여부와 상관 없이 데이터를 emit 하는 Hot 으로 구분할 수 있음.
+         */
         String[] singers = {"Singer A", "Singer B", "Singer C", "Singer D", "Singer E"};
 
         log.info("# Begin concert:");
